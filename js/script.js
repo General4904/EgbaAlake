6773cacd8148f4e50ec9fd862c18226d0c17e19e
Generalissimo <110818802+General4904@users.noreply.github.com>
2022-12-15 12:09:55 +0100

    Added new contents

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
-function welcome(){
-	alert("Hello there, welcome to Egbaalake Radio")
-}
+let newLink = document.createElement("a");
 
+let par =  document.getElementById("Yoruba");
+
+let firstParagraph = par[0];
+
+firstParagraph.appendChild(newLink);
+
+newLink.setAttribute("href", "#");
+newLink.setAttribute("class", "newlink");
+
+newLink.innerHTML = "Read more"
+
+// // // for (let i = 0; i < par.length; i++) {
+// // //     if (i === 0) {
+// // //         continue;
+// // //     };
+// // // }
+
+// // function revealCopy(params) {
+// //     console.log("Clicked!")
+// // }
+
+// newLink.addEventListener("click", revealCopy)
+
+// function welcome(){
+//     alert("Hello World")
+// }
