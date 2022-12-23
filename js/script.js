685f8bf3752143b63ad8e888a67d6797ddf4b02c
Generalissimo <110818802+General4904@users.noreply.github.com>
2022-12-23 09:54:34 +0100

    Completed the yoruba history
    And added a new page for the rest of the history

--- js/script.js
+++ js/script.js
@@ -1,28 +1,5 @@
-let newLink = document.createElement("a");
+// const date = new Date();
 
-let par =  document.getElementById("Yoruba");
+// let displayDate = document.getElementById("time");
+// displayDate.innerHTML = date
 
-let firstParagraph = par[0];
-
-firstParagraph.appendChild(newLink);
-
-newLink.setAttribute("href", "#");
-newLink.setAttribute("class", "newlink");
-
-newLink.innerHTML = "Read more"
-
-// // // for (let i = 0; i < par.length; i++) {
-// // //     if (i === 0) {
-// // //         continue;
-// // //     };
-// // // }
-
-// // function revealCopy(params) {
-// //     console.log("Clicked!")
-// // }
-
-// newLink.addEventListener("click", revealCopy)
-
-// function welcome(){
-//     alert("Hello World")
-// }
