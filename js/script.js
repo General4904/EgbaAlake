14ea504f63c6ceb2390b0e12d4a2d9dffb099cc7
Generalissimo <110818802+General4904@users.noreply.github.com>
2023-03-09 09:15:19 +0100

    Changed the footer appearance, added new js code

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 let clock = document.getElementById("time");
 
-setInterval(() =>{
-    let x = new Date();
+setInterval(() => {
+	let x = new Date();
 
+	clock.innerHTML = x.toLocaleDateString() + "  |  " + x.toLocaleTimeString();
+}, 1000);
 
-    clock.innerHTML = x.toLocaleDateString() + "  |  " + x.toLocaleTimeString()
-}, 1000)
+//<small> &copy; 2022 - v2.3.0 </small>
 
- 
+let version = document.getElementById("copy");
+
+version.innerHTML = "<small> &copy; 2022 - v2.4.0 </small>";
